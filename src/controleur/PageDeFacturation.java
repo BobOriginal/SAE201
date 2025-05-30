685c204d04f64cbc5a4049a68834e5b48dc1c95a
Gavroche <d.elouan@laposte.net>
2025-05-30 09:47:23 +0200

    :bug: réparation de ma branche, enfin je crois

---
+++ src/controleur/PageDeFacturation.java
@@ -0,0 +1,12 @@
+package controleur;
+
+import java.io.File;
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+public class PageDeFacturation {
+
+
+}
